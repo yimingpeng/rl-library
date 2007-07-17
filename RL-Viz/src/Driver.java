e4d5e0c6a229dab5ed4c0633f5dce8f2fcd75947
bttanner <bttanner@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-07-17 03:35:46 +0000

    Working ish prototype

--- RL-Viz/src/Driver.java
+++ RL-Viz/src/Driver.java
@@ -1,7 +1,51 @@
+import rlglue.*;
+import java.io.IOException;
 
-public class Driver {
+public class Driver
+{
+    protected static final int kNumEpisodes = 100;
+    protected static int rlNumSteps[];
+    protected static double rlReturn[];
 
-    public static void main(String[] args) {
-    	System.out.println("This will be the basis for the RL-Glue Viz");
+    protected static void run(int numEpisodes) throws IOException
+    {
+	/*run for num_episode number of episodes and store the number of steps and return from each episode*/        	for(int x = 0; x < numEpisodes; ++x) {
+	    RLGlue.RL_episode(0);
+	    System.out.println(RLGlue.RL_num_steps());
+	    
+	    rlNumSteps[x] = RLGlue.RL_num_steps();
+	    rlReturn[x] = RLGlue.RL_return();
+	}
     }
+    
+    public static void main(String [] args) throws IOException {
+	double avgSteps = 0.0;
+	double avgReturn = 0.0;
+	
+	rlNumSteps = new int[Driver.kNumEpisodes];
+	rlReturn = new double[Driver.kNumEpisodes];
+
+	/*basic main loop*/
+	
+	RLGlue.RL_init();
+	run(kNumEpisodes);
+	RLGlue.RL_cleanup();
+	
+	/*add up all the steps and all the returns*/
+	for (int i = 0; i < Driver.kNumEpisodes; ++i) {
+	    avgSteps += rlNumSteps[i];
+	    avgReturn += rlReturn[i];
+	}
+	
+	/*average steps and returns*/
+	avgSteps /= (double)Driver.kNumEpisodes;
+	avgReturn /= (double)Driver.kNumEpisodes;
+	
+	/*print out results*/
+	System.out.println("\n-----------------------------------------------\n");
+	System.out.println("Number of episodes: " + Driver.kNumEpisodes);
+	System.out.println("Average number of steps per episode: " +  avgSteps);
+	System.out.println("Average return per episode: " + avgReturn);
+	System.out.println("-----------------------------------------------\n");
+    }   
 }
