046482376b4184924b820ab3e79c0ced3b69bc73
bttanner <bttanner@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-07-17 01:06:52 +0000

    I think mountainCar works

---
+++ trunk/MountainCar/src/EnvironmentBase.java
@@ -0,0 +1,20 @@
+import rlglue.Environment;
+import rlglue.Observation;
+import rlglue.Reward_observation;
+
+
+public abstract class EnvironmentBase implements Environment {
+	abstract Observation makeObservation();
+	
+	protected Reward_observation makeRewardObservation(double reward, boolean isTerminal){
+		Reward_observation RO=new Reward_observation();
+		RO.o=makeObservation();
+		RO.r=reward;
+		
+		RO.terminal=1;
+		if(!isTerminal)
+			RO.terminal=0;
+
+		return RO;
+	}
+}
