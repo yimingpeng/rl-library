449963bea32f445c64a3a5f979e62f0fea276842
bttanner <bttanner@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-07-18 08:01:15 +0000

    RLViz basics up and running

---
+++ RL-VizLib/src/messaging/agent/AgentMessageType.java
@@ -0,0 +1,14 @@
+package messaging.agent;
+
+public enum AgentMessageType{
+	kAgentResponse(0),
+	kAgentQueryValuesForObs(1),
+	kAgentCustom(2);
+	
+	private final int id;
+	
+	AgentMessageType(int id){
+        this.id = id;
+    }
+    public int id()   {return id;}
+}
