4e13299e8ff80082fca76788b8f5337ebcdb0801
bttanner <bttanner@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-07-20 04:23:07 +0000

    Adding value function stuff to RL-Viz

---
+++ trunk/RL-Viz/src/ValueFunctionDataProvider.java
@@ -0,0 +1,13 @@
+import java.awt.Point;
+import java.util.Vector;
+
+import rlglue.Observation;
+
+
+public interface ValueFunctionDataProvider {
+	
+	public double getMinValueForDim(int whichDimension);
+	public double getMaxValueForDim(int whichDimension);
+	public Vector<Observation> getQueryObservations(Vector<Observation> theQueryStates);
+	public Vector<Double> queryAgentValues(Vector<Observation> theQueryObs);	
+}
