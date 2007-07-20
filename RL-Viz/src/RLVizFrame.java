a9b075ae12727fb3a94a5fbb7bd36a3416118820
burnt.rain <burnt.rain@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-07-20 23:12:17 +0000

    classpath

--- RL-Viz/src/RLVizFrame.java
+++ RL-Viz/src/RLVizFrame.java
@@ -14,5 +14,31 @@ import rlglue.Observation;
 
 public class RLVizFrame extends JFrame implements ValueFunctionDataProvider {
 
+	public double getMaxValueForDim(int whichDimension) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public double getMinValueForDim(int whichDimension) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Vector<Observation> getQueryObservations(
+			Vector<Observation> theQueryStates) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Vector<Double> queryAgentValues(Vector<Observation> theQueryObs) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setRanges(Vector<Double> mins, Vector<Double> maxs) {
+		// TODO Auto-generated method stub
+		
+	}
+
 
 }
