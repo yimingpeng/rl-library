01d9f47acad0c22e4eaabe8e7c3da98b84ac9d1d
bttanner <bttanner@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-07-28 05:21:27 +0000

    Initial import.
    Quick start at writing a java based trainer.

---
+++ JavaTrainer/src/JavaTrainer.java
@@ -0,0 +1,27 @@
+import rlVizLib.messaging.environmentShell.EnvShellLoadRequest;
+import rlglue.RLGlue;
+
+
+public class JavaTrainer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String theEnv="MountainCar";
+		EnvShellLoadRequest.Execute("MountainCar");
+		//This is like sending the following, which goes to the environmentShell Loader and loads the theEnv.
+		//RL_env_message("TO=1 FROM=0 CMD=2 VALTYPE=1 VALS="+theEnv);
+		
+		RLGlue.RL_init();
+		
+		int numEpisodes=10;
+	
+		for(int i=0;i<numEpisodes;i++){
+			RLGlue.RL_episode(0);
+			System.out.println("Steps: "+RLGlue.RL_num_steps());
+		}
+		RLGlue.RL_cleanup();
+	}
+
+}
