b84cc7544444394cbad36e7964bad8fbb0d05554
andrewbutcher <andrewbutcher@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-08-03 22:48:49 +0000

    BugFix: The Java task spec parser was broken. This is a rewrite.

--- trunk/RL-VizLib/src/rlVizLib/utilities/TaskSpecObject.java
+++ trunk/RL-VizLib/src/rlVizLib/utilities/TaskSpecObject.java
@@ -1,18 +1,63 @@
 package rlVizLib.utilities;
 public class TaskSpecObject
 {
-    public float version;			
+    public double version;			
     public char episodic;			
     public int obs_dim;	
 	public int num_discrete_obs_dims;
 	public int num_continuous_obs_dims;		
     public char [] obs_types;	    
-    public float [] obs_mins;           
-    public float [] obs_maxs;			
+    public double [] obs_mins;           
+    public double [] obs_maxs;			
     public int action_dim;
 	public int num_discrete_action_dims;
 	public int num_continuous_action_dims;			
     public char [] action_types;		
-    public float [] action_mins;		
-    public float [] action_maxs;	
+    public double [] action_mins;		
+    public double [] action_maxs;	
+    
+    public String toString()
+    {
+    	String obs_types_string = "";
+    	for (int i = 0; i < obs_types.length; ++i)
+    		obs_types_string += obs_types[i] + " ";
+    	
+    	String obs_mins_string = "";
+    	for (int i = 0; i < obs_mins.length; ++i)
+    		obs_mins_string += obs_mins[i] + " ";
+    	
+    	String obs_maxs_string = "";
+    	for (int i = 0; i < obs_maxs.length; ++i)
+    		obs_maxs_string += obs_maxs[i] + " ";
+    	
+       	String action_types_string = "";
+    	for (int i = 0; i < action_types.length; ++i)
+    		action_types_string += action_types[i] + " ";
+    	
+    	String action_mins_string = "";
+    	for (int i = 0; i < action_mins.length; ++i)
+    		action_mins_string += action_mins[i] + " ";
+    	
+    	String action_maxs_string = "";
+    	for (int i = 0; i < action_maxs.length; ++i)
+    		action_maxs_string += action_maxs[i] + " ";
+ 
+    	
+    	String taskSpecObject = "version: " + version + "\n" +
+    		   "episodic: " + episodic + "\n" + 
+    		   "obs_dim: " + obs_dim + "\n" + 
+    		   "num_discrete_obs_dims: " + num_discrete_obs_dims + "\n" +
+    		   "num_continuous_obs_dims: " + num_continuous_obs_dims + "\n" +
+    		   "obs_types: " + obs_types_string + "\n" +
+    		   "obs_mins: " + obs_mins_string + "\n" +
+    		   "obs_maxs: " + obs_maxs_string + "\n" +
+    		   "action_dim: " + action_dim + "\n" + 
+    		   "num_discrete_action_dims: " + num_discrete_action_dims + "\n" +
+    		   "num_continuous_action_dims: " + num_continuous_action_dims + "\n" +
+    		   "action_types: " + action_types_string + "\n" +
+    		   "action_mins: " + action_mins_string + "\n" +
+    		   "action_maxs: " + action_maxs_string + "\n";
+    	
+    	return taskSpecObject;
+    }
 }
--- trunk/RL-VizLib/src/rlVizLib/utilities/TaskSpecParser.java
+++ trunk/RL-VizLib/src/rlVizLib/utilities/TaskSpecParser.java
@@ -1,128 +1,201 @@
 package rlVizLib.utilities;
 
+import java.util.StringTokenizer;
+
+/* This shouldn't all be static, and TaskSpecObject and TaskSpecParser should be merged.
+ * The parsing should be started by the TaskSpecParser constructor.
+ */
+
 public class TaskSpecParser
-{        
-    public static TaskSpecObject parse(String ts)
-    {
-    	System.out.println("Parsing task spec: "+ts);
-	TaskSpecObject tsObject = new TaskSpecObject();
-	String [] tmp = null;
-	String [] tmp2 = null;
-	String [] tmp3 = null;
-	int discrete =0;
-	int continuous =0;
-
-	try 
+{	
+	protected static void parseObservationTypesAndDimensions(String obsTypesString, TaskSpecObject specObject) throws Exception
 	{
-	    tmp = ts.split(":");
-	   
-	    tsObject.version = Float.parseFloat(tmp[0]);    
-	    tsObject.episodic = (tmp[1].trim()).charAt(0);
+		// Discard the [ ] around the types string
+		obsTypesString = obsTypesString.substring(1, obsTypesString.length() - 1);
+				
+		// Split up the observation types
+		StringTokenizer obsTypesTokenizer = new StringTokenizer(obsTypesString, ",");
+		
+		/* Parse the data out of obsTypesString.
+		 * Allocate and fill the obs_types array, and set the number 
+		 * of discrete and continuous observation dimensions.
+		 */
+		specObject.obs_types = new char[obsTypesTokenizer.countTokens()];
+		specObject.num_discrete_obs_dims   = 0;
+		specObject.num_continuous_obs_dims = 0;
+		
+		/* We get the observation type from the tokenizer, 
+		 * add it to the obs_types array, and update the discrete and continuous dimensions
+		 */
+		int currentObservationTypeIndex = 0;
+		while (obsTypesTokenizer.hasMoreTokens())
+		{
+			char obsType = obsTypesTokenizer.nextToken().charAt(0);
+			specObject.obs_types[currentObservationTypeIndex] = obsType;
+			switch(obsType)
+			{
+			case 'i':
+				specObject.num_discrete_obs_dims += 1;
+				break;
+				
+			case 'f':
+				specObject.num_continuous_obs_dims += 1;
+				break;
+				
+			default: 
+				throw new Exception ("Unknown Observation Type: " + obsType);
+			}
+			currentObservationTypeIndex += 1;
+		}
+	}
 	
-	    tmp2 = tmp[2].split("_");
-
-	    tsObject.obs_dim = Integer.parseInt(tmp2[0]);
-	    tsObject.obs_mins = new float[tsObject.obs_dim];           
-	    tsObject.obs_maxs = new float[tsObject.obs_dim]; 
-	    tsObject.obs_types = new char[tsObject.obs_dim];
-        
-	    tmp3 = tmp2[1].split(",");
-	    
-	    if(tsObject.obs_dim == 1){
-		tsObject.obs_types[0] = tmp3[0].substring(1,tmp3[0].length()-1).trim().charAt(0);
-		if(tsObject.obs_types[0] == 'i')
-			discrete++;
-		else
-			continuous++;
-		}
-	    else
-	    {
-		tmp3[0] = tmp3[0].substring(1,tmp3[0].length()); 
-		tmp3[tsObject.obs_dim-1] = tmp3[tsObject.obs_dim-1].substring(0, tmp3[tsObject.obs_dim-1].length()-1);
-		
-		for(int i = 0; i < tsObject.obs_dim; i++){
-		    tsObject.obs_types[i] = (tmp3[i].trim()).charAt(0);
-			if(tsObject.obs_types[i] == 'i')
-				discrete++;
-			else
-				continuous++;
-		}
-	    }
-
-	    //Obs Dim Loop
-	    for(int i = 2; i < tsObject.obs_dim + 2; i++)
-	    {
-		tmp3 = null;
-		tmp3 = tmp2[i].split(",");
-		System.out.println("tmp2["+i+"] = "+tmp2[i]);
-		tmp3[0] = tmp3[0].substring(1,tmp3[0].length());
-		tmp3[1] = tmp3[1].substring(0, tmp3[1].length()-1);
-		tsObject.obs_mins[i-2] = Float.parseFloat(tmp3[0]);
-		tsObject.obs_maxs[i-2] = Float.parseFloat(tmp3[1]);
-	    }
-		
-		tsObject.num_discrete_obs_dims = discrete;
-		tsObject.num_continuous_obs_dims = continuous;
-		discrete =0;
-		continuous =0;
-    
-	    tmp2 = null;
-	    tmp2 = tmp[3].split("_");
-	    
-	    tsObject.action_dim = Integer.parseInt(tmp2[0]);
-	    tsObject.action_mins = new float[tsObject.action_dim];           
-	    tsObject.action_maxs = new float[tsObject.action_dim]; 
-	    tsObject.action_types = new char[tsObject.action_dim];
-	    
-	    tmp3 = tmp2[1].split(",");
-	    
-	    if(tsObject.action_dim == 1){
-		tsObject.action_types[0] = tmp3[0].substring(1,tmp3[0].length()-1).trim().charAt(0);        
-		if(tsObject.obs_types[0] == 'i')
-			discrete++;
-		else
-			continuous++;
-		}
-	    else
-	    {   
-		tmp3[0] = tmp3[0].substring(1,tmp3[0].length()); 
-		tmp3[tsObject.action_dim-1] = tmp3[tsObject.action_dim-1].substring(0, tmp3[tsObject.action_dim-1].length()-1);
-		
-		for(int i = 0; i < tsObject.action_dim; ++i){
-		    tsObject.action_types[i] = (tmp3[i].trim()).charAt(0);
-			if(tsObject.obs_types[0] == 'i')
-				discrete++;
-			else
-				continuous++;
-	    }
-	    }
-		
-	    for(int i = 2; i < tsObject.action_dim + 2; ++i)
-	    {
-		tmp3 = null;
-		tmp3 = tmp2[i].split(",");
-		
-		System.out.println("I think the action part of the task spec is: "+tmp2[i]);
-		tmp3[0] = tmp3[0].substring(1,tmp3[0].length());
-		tmp3[1] = tmp3[1].substring(0, tmp3[1].length()-1);
-		tsObject.action_mins[i-2] = Float.parseFloat(tmp3[0]);
-		tsObject.action_maxs[i-2] = Float.parseFloat(tmp3[1]);
-	    }
-	    
-		tsObject.num_discrete_action_dims = discrete;
-		tsObject.num_continuous_action_dims = continuous;
-		discrete =0;
-		continuous =0;
-	}
-	catch (Exception e)
+	protected static void parseObservationRanges(StringTokenizer observationTokenizer, TaskSpecObject specObject)
 	{
-	    System.out.println("\nError: Your Task Specification has incorrect format! \nExiting ... \n");
-	    System.out.println(e);
-	    e.printStackTrace();
-	    System.exit(0);
+		// Now we can allocate our obs mins and obs maxs arrays
+		specObject.obs_mins = new double[specObject.obs_types.length];
+		specObject.obs_maxs = new double[specObject.obs_types.length];
+		int currentRange = 0;
+		while(observationTokenizer.hasMoreTokens())
+		{
+			String observationRange = observationTokenizer.nextToken();
+			observationRange = observationRange.substring(1, observationRange.length() - 1);
+			StringTokenizer rangeTokenizer = new StringTokenizer(observationRange, ",");	
+			
+			specObject.obs_mins[currentRange] = Double.parseDouble(rangeTokenizer.nextToken());
+			specObject.obs_maxs[currentRange] = Double.parseDouble(rangeTokenizer.nextToken());
+			currentRange += 1;
+		}		
 	}
 
-	return tsObject;
-    }
+	protected static void parseActionTypesAndDimensions(String actionTypesString, TaskSpecObject specObject) throws Exception
+	{
+		// Discard the [ ] around the types string
+		actionTypesString = actionTypesString.substring(1, actionTypesString.length() - 1);
+		
+		// Split up the observation types
+		StringTokenizer actionTypesTokenizer = new StringTokenizer(actionTypesString, ",");
+		
+		/* Parse the data out of obsTypesString.
+		 * Allocate and fill the obs_types array, and set the number 
+		 * of discrete and continuous observation dimensions.
+		 */
+		specObject.action_types = new char[actionTypesTokenizer.countTokens()];
+		specObject.num_discrete_action_dims = 0;
+		specObject.num_continuous_action_dims = 0;
+		
+		/* We get the observation type from the tokenizer, 
+		 * add it to the obs_types array, and update the discrete and continuous dimensions
+		 */
+		int currentActionTypeIndex = 0;
+		while (actionTypesTokenizer.hasMoreTokens())
+		{
+			char actionType = actionTypesTokenizer.nextToken().charAt(0);
+			specObject.action_types[currentActionTypeIndex] = actionType;
+			switch(actionType)
+			{
+			case 'i':
+				specObject.num_discrete_action_dims += 1;
+				break;
+				
+			case 'f':
+				specObject.num_continuous_action_dims += 1;
+				break;
+				
+			default: 
+				throw new Exception ("Unknown Action Type: " + actionType);
+			}
+			currentActionTypeIndex += 1;
+		}
+	}
+	
+	protected static void parseActionRanges(StringTokenizer actionTokenizer, TaskSpecObject specObject)
+	{
+		// Now we can allocate our obs mins and obs maxs arrays
+		specObject.action_mins = new double[specObject.action_types.length];
+		specObject.action_maxs = new double[specObject.action_types.length];
+		int currentRange = 0;
+		while(actionTokenizer.hasMoreTokens())
+		{
+			String actionRange = actionTokenizer.nextToken();
+			actionRange = actionRange.substring(1, actionRange.length() - 1);
+			StringTokenizer rangeTokenizer = new StringTokenizer(actionRange, ",");	
+			
+			specObject.action_mins[currentRange] = Double.parseDouble(rangeTokenizer.nextToken());
+			specObject.action_maxs[currentRange] = Double.parseDouble(rangeTokenizer.nextToken());
+			currentRange += 1;
+		}		
+	}
+	
+	protected static void parseObservations(String observationString, TaskSpecObject specObject) throws Exception
+	{
+
+		/* Break the observation into its three component parts
+		 * The number of dimensions to the observation
+		 * The types of the observation
+		 * The ranges of the observations
+		 */
+		StringTokenizer observationTokenizer = new StringTokenizer(observationString, "_");
+		String obsDimensionString = observationTokenizer.nextToken();
+		String obsTypesString = observationTokenizer.nextToken();
+		
+		specObject.obs_dim = Integer.parseInt(obsDimensionString);
+		parseObservationTypesAndDimensions(obsTypesString, specObject);
+		parseObservationRanges(observationTokenizer, specObject);
+	}
+	
+	protected static void parseActions(String actionString, TaskSpecObject specObject) throws Exception
+	{
+		StringTokenizer actionTokenizer = new StringTokenizer(actionString, "_");
+		String actionDimensionString = actionTokenizer.nextToken();
+		String actionTypesString = actionTokenizer.nextToken();	
+		
+		specObject.action_dim = Integer.parseInt(actionDimensionString);
+		parseActionTypesAndDimensions(actionTypesString, specObject);
+		parseActionRanges(actionTokenizer, specObject);
+	}
+
+
+	public static TaskSpecObject parse(String taskSpecString) throws Exception
+	{
+		TaskSpecObject specObject = new TaskSpecObject();
+		
+		/* Break the task spec into its four component parts
+		 * The version number
+		 * The task style (episodic/continuous)
+		 * The observation data
+		 * The action data 
+		 */
+		
+		StringTokenizer tokenizer = new StringTokenizer(taskSpecString, ":");		
+
+		String versionString = tokenizer.nextToken();
+		String taskStyle = tokenizer.nextToken();
+		String observationString = tokenizer.nextToken();
+		String actionString = tokenizer.nextToken();	
+
+		specObject.version = Double.parseDouble(versionString);
+		specObject.episodic = taskStyle.charAt(0);
+
+		parseObservations(observationString, specObject);
+		parseActions(actionString, specObject);
+		
+		return specObject;
+	}
+	
+	public static void main(String [] args) throws Exception
+	{
+		double minPosition = -1.2;
+		double maxPosition =  0.6;
+		double minVelocity = -0.07;    
+		double maxVelocity =  0.07;
+	    
+		String taskSpec = "1:e:2_[f,f]_["+minPosition+","+maxPosition+"]_["+minVelocity+","+maxVelocity+"]:1_[i]_[0,2]";
+
+		System.err.println(taskSpec);
+		
+		TaskSpecObject taskObject = TaskSpecParser.parse(taskSpec);
+		
+		System.err.println(taskObject);
+	}
 }
-    
+
