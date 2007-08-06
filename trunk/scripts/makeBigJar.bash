80989c62bfd76cae8705fa1f66fb111f9d35affd
bttanner <bttanner@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-08-06 16:56:39 +0000

    Fixed up the scripts for the training package

--- trunk/scripts/makeBigJar.bash
+++ trunk/scripts/makeBigJar.bash
@@ -1,18 +1,31 @@
 #!/bin/bash
 
+#This script should be fun from the workspace root directory
 echo "============================"
 echo "Running makeBigJar.bin"
+echo "Is Comprised of: "
+echo " -- RL Glue Codecs from the rl-glue project"
+echo " -- Vizlib files from the RL-Viz project"
+echo " -- A visualizer from the RL-Viz project"
+echo " -- A dynamic environment loader for Java compatible with RL-Viz"
 echo "============================"
 
 rm -Rf bigJarTmp
 mkdir bigJarTmp
 mkdir bigJarTmp/bin/
 
-cp -R ../RLGlueJava/bin/* bigJarTmp/bin/
-cp -R ../RL-VizLib/bin/* bigJarTmp/bin/
-cp -R ../RL-Viz/bin/* bigJarTmp/bin/
+echo " --Getting the .class files together"
+cp -R ./RLGlueJava/bin/* bigJarTmp/bin/
+cp -R ./RL-VizLib/bin/* bigJarTmp/bin/
+cp -R ./RL-Viz/bin/* bigJarTmp/bin/
+cp -R ./EnvironmentShell/bin/* bigJarTmp/bin/
 
 cd bigJarTmp/bin/
-jar -cvf ../RL-Train.jar *
+echo " --Making RL-Train.jar (quietly)"
+jar -cf ../../RL-Train.jar *
+cd ../../
+rm -Rf bigJarTmp
 #jar -cvf ../../trainingPack/bin/RL-Viz.jar *
 
+echo " -- makeBigJar.bash completed"
+echo "------------------------------"
--- trunk/scripts/makeTrainingPackage.bash
+++ trunk/scripts/makeTrainingPackage.bash
@@ -1,56 +1,93 @@
-#/bin/bash
+#!/bin/bash
+
+#################
+## Create directories and stuff
+#################
+echo "============================"
+echo "Running makeTrainingPackage.bin"
+echo "============================"
+
+echo " * Making directories..."
 cd ../
 rm -Rf trainingPack
 mkdir trainingPack
 mkdir trainingPack/bin
-mkdir trainingPack/bin/environmentJars
-mkdir trainingPack/bin/agent
+mkdir trainingPack/bin/environments
+
+mkdir trainingPack/bin/agents
+mkdir trainingPack/bin/agents/GenericSarsaLambdaJava
+
+mkdir trainingPack/bin/trainingExperiments
+mkdir trainingPack/bin/trainingExperiments/trainJava
+
 mkdir trainingPack/src
+mkdir trainingPack/src/agents
+mkdir trainingPack/src/agents/GenericSarsaLambdaJava
+
+mkdir trainingPack/src/trainingExperiments
+mkdir trainingPack/src/trainingExperiments/trainJava
+
 mkdir trainingPack/src/environments
 mkdir trainingPack/src/environments/mountainCar
+mkdir trainingPack/src/environments/Tetrlais
 
-#Copy the Glue Executable
-cp ~/ProgrammingProjects/rl-glue/Examples/Network_Java/bin/RL_glue trainingPack/bin/
-#Copy the RL-Glue source and makefile into trainingPack/RL-Glue/
+echo " * Building the big jar..."
+./scripts/makeBigJar.bash
 
-#Copy the RL-Glue Jar
-mv RL-Glue.jar trainingPack/bin/
+echo " * Copying that jar to the bin directory of trainingPack..."
+cp ./RL-Train.jar trainingPack/bin/
+
+
+echo " * Exporting a copy of rl-glue from google code repository to the training pack (quietly)"
+#Copy the RL-Glue source
+svn export -q https://rl-glue.googlecode.com/svn/trunk/ trainingPack/rl-glue
+#cp ~/ProgrammingProjects/rl-glue/Examples/Network_Java/bin/RL_glue trainingPack/bin/
+
 
 
 #################
 ## MOUNTAIN CAR
 #################
+echo " * Copying mountainCar source code to the training pack"
 ##Copy the source
 cd MountainCar
 cp -R src/* ../trainingPack/src/environments/mountainCar/
 
+echo " * Building the mountainCar jar and moving it to the trainingPack to the training pack"
 ##Make the MountainCar Jar
 cd bin
-jar -cvf ../../trainingPack/bin/environmentJars/MountainCar.jar ./MountainCar/*.class
+jar -cf ../../trainingPack/bin/environments/MountainCar.jar ./MountainCar/*.class
 cd ../../
 
-#Copy the bin files for SarsaLambda
-cp -R GenericSarsaLambda/bin/* trainingPack/bin/agent/
+#################
+## Tetrlais
+#################
+echo " * Copying Tetris source code to the training pack"
+##Copy the source
+cd Tetrlais
+cp -R src/* ../trainingPack/src/environments/Tetrlais/
 
-
-#Make the VizLib Jar
-cd RL-VizLib/bin/
-jar -cvf ../../trainingPack/bin/RL-VizLib.jar *
+echo " * Building the Tetris jar and moving it to the trainingPack to the training pack"
+cd bin
+jar -cf ../../trainingPack/bin/environments/Tetrlais.jar ./Tetrlais/*.class
 cd ../../
 
-#Make the Viz Jar
-cd RL-Viz/bin/
-jar -cvf ../../trainingPack/bin/RL-Viz.jar *
-cd ../../
 
-#Make the EnvShell Jar
-cd EnvironmentShell/bin/
-jar -cvf ../../trainingPack/bin/EnvShell.jar ./environmentShell/*.class
-cd ../../
+#################
+## GenericSarsaLambdaJava
+#################
+#Copy source and binaries for GenericSarsaLambda
+echo " * Copying to source and binaries for GenericSarsaLamda"
+cp -R GenericSarsaLambda/src/* trainingPack/src/agents/GenericSarsaLambdaJava/
+cp -R GenericSarsaLambda/bin/* trainingPack/bin/agents/GenericSarsaLambdaJava/
 
-#Copy the EnvTrainer
-cp JavaTrainer/src/*.java trainingPack/src/
-cp JavaTrainer/bin/*.class trainingPack/bin/
+#################
+## Java Trainer
+#################
+echo " * Copying to source and binaries for Java Trainer"
+cp JavaTrainer/src/*.java trainingPack/src/trainingExperiments/trainJava/
+cp JavaTrainer/bin/*.class trainingPack/bin/trainingExperiments/trainJava/
 cp scripts/runTrainer.bash trainingPack/
+cp scripts/runViz.bash trainingPack/
 
 
