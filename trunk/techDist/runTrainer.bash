cc76f8f0d6df9af354bea6c58d93a80d529baf9d
andrewbutcher <andrewbutcher@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-08-09 21:29:42 +0000

    BugFix: Brian made a change to runTrainer.bash -- there was a bug in it.

--- trunk/techDist/runTrainer.bash
+++ trunk/techDist/runTrainer.bash
@@ -3,11 +3,7 @@
 
 killall RL_glue
 
-cd ../trainingPack
-pwd
 
-#Just for now
-cp ~/ProgrammingProjects/rl-glue/Examples/Network_Java/bin/RL_glue bin/RL_glue
 export RLTRAINPATH=./bin/RL-Train.jar
 export AGENTPATH=./bin/agents/GenericSarsaLambdaJava/
 export EXPERIMENTPATH=./bin/trainingExperiments/trainJava/
