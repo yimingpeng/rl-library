33c3f2aa4ce2df3e0e28be2303478bd61bf71b18
bttanner <bttanner@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-08-10 21:58:44 +0000

    C trainer ready to roll

--- trunk/techDist/src/trainingExperiments/trainCpp/trainCpp.cpp
+++ trunk/techDist/src/trainingExperiments/trainCpp/trainCpp.cpp
@@ -2,57 +2,36 @@
 #include <iostream>
 #include <string>
 #include <RL_glue.h>
+#include <vector>
+#include "ParameterHolder.h"
+#include "CommunicationStuff.h"
 
 using std::string;
 using std::cerr;
 
-const int kNumTrials = 10000;
-const int kNumSteps  = 1000;
-
-#define BENCHMARK 0
-#define ENVSHELL 1
-#define LISTQUERY 1
-#define NOVALUE 3
-
 
 int main(int argc, char *argv[])
 {
-    int sum = 0;
-//		string theEnv="Tetrlais";
-    string theEnv = "MountainCar";
+		string theEnv="Tetrlais";
+//    string theEnv = "MountainCar";
 
-	char theRequest[128]={0};
-	sprintf(theRequest,"TO=%d FROM=%d CMD=%d VALTYPE=%d VALS=NULL",ENVSHELL, BENCHMARK, LISTQUERY, NOVALUE);
-	std::cout<<"The message is: "<<theRequest<<std::endl;
-
-
-	std::string theResponse=std::string(RL_env_message(theRequest));
-	std::string::size_type lastColonPos = theResponse.rfind (":",0);
-	std::string thePayLoad=theResponse.substr(lastColonPos+1);
+	ParameterHolder *theParams = getParameterHolderForEnvironment(theEnv);
 	
-	std::cout<<"The payLoad is: "<<thePayLoad<<std::endl;
+	//IF env == MountainCa
+//	theParams->setBoolParam("randomStartStates",true);
 	
-	
-	
-		// EnvShellListResponse ListResponse = EnvShellListRequest.Execute();
-		// 
-		// int thisEnvIndex=ListResponse.getTheEnvList().indexOf(theEnv);
-		// 
-		// ParameterHolder p = ListResponse.getTheParamList().get(thisEnvIndex);
-		// 
-		// System.out.println("Running with Parameter Settings: "+p);
-		// 
-		// EnvShellLoadRequest.Execute(theEnv,p);
-		// 
+	loadEnvironment(theEnv,theParams);
 
-
-    cerr << "Running with Parameter Settings: " << "<put parameter holder here>\n";
     RL_init();
-    for (int i = 0; i < kNumTrials; ++i)
+
+    int sum = 0;
+	unsigned int numEpisodes = 5000;
+    for (int i = 0; i < numEpisodes; ++i)
     {
-        RL_episode(kNumSteps);
+        RL_episode(100000);
         sum += RL_num_steps();
-        if ( i % 500 == 0 )
+
+        if ( (i+1) % 100 == 0 )
         {
             cerr << "Running episode: " << i << " total steps in last bunch is: " << sum << "\n";
             sum = 0;
