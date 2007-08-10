ba85eb061e2ebcf46f64521a50f40ea62b189388
bttanner <bttanner@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-08-10 22:04:40 +0000

    latest submissions

--- trunk/Tetrlais/src/Tetrlais/Tetrlais.java
+++ trunk/Tetrlais/src/Tetrlais/Tetrlais.java
@@ -47,6 +47,7 @@ public class Tetrlais extends EnvironmentBase implements RLVizEnvInterface {
 			if(!p.isNull()){
 				width=p.getIntParam("Width");
 				height=p.getIntParam("Height");
+				System.out.println("Running tetris with: "+width+" x "+height);
 				if(p.getBooleanParam("LongBlock"))	possibleBlocks.add(TetrlaisPiece.makeLine());
 				if(p.getBooleanParam("SquareBlock"))	possibleBlocks.add(TetrlaisPiece.makeSquare());
 				if(p.getBooleanParam("TriBlock"))	possibleBlocks.add(TetrlaisPiece.makeTri());
@@ -212,13 +213,11 @@ public class Tetrlais extends EnvironmentBase implements RLVizEnvInterface {
 	/*RL-Viz Methods*/
 	@Override
 	protected Observation makeObservation() {
-		// TODO Auto-generated method stub
 		return gameState.get_observation();
 	}
 	/*End of RL-Viz Methods*/
 
 	public RLVizVersion getTheVersionISupport() {
-		// TODO Auto-generated method stub
 		return new RLVizVersion(1,0);
 	}
 
