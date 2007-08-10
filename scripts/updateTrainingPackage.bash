a6729f1257b77a21859b9011b5a3b200b788f4e3
bttanner <bttanner@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-08-10 16:05:47 +0000

    script is no longer destructive

--- scripts/updateTrainingPackage.bash
+++ scripts/updateTrainingPackage.bash
@@ -4,7 +4,7 @@
 ## Create directories and stuff
 #################
 echo "============================"
-echo "Running updateTrainingPackage.bin"
+echo "Running updatetechDistage.bin"
 echo "============================"
 cd ..
 
@@ -12,7 +12,7 @@ echo " * Building the big jar..."
 ./scripts/makeBigJar.bash
 
 echo " * Copying that jar to the bin directory of training pack..."
-mv -f ./RL-Train.jar trainingPack/bin/
+mv -f ./RL-Train.jar techDist/bin/
 
 
 #################
@@ -21,13 +21,17 @@ mv -f ./RL-Train.jar trainingPack/bin/
 echo " * Copying mountainCar source code to the training pack"
 ##Copy the source
 cd MountainCar
-rm -Rf ../trainingPack/src/environments/mountainCar/*
-cp -R src/* ../trainingPack/src/environments/mountainCar/
-echo " * Building the mountainCar jar and moving it to the trainingPack to the training pack"
+rm ../techDist/src/environments/mountainCar/*.class
+rm ../techDist/src/environments/mountainCar/MountainCar/*.class
+
+cp src/*.java ../techDist/src/environments/mountainCar/
+cp src/MountainCar/*.java ../techDist/src/environments/mountainCar/MountainCar/
+
+echo " * Building the mountainCar jar and moving it to the techDist to the training pack"
 ##Make the MountainCar Jar
 cd bin
-rm ../../trainingPack/bin/environments/MountainCar.jar
-jar -cf ../../trainingPack/bin/environments/MountainCar.jar ./MountainCar/*.class
+rm ../../techDist/bin/environments/MountainCar.jar
+jar -cf ../../techDist/bin/environments/MountainCar.jar ./MountainCar/*.class
 cd ../../
 
 #################
@@ -36,14 +40,35 @@ cd ../../
 echo " * Copying Tetris source code to the training pack"
 ##Copy the source
 cd Tetrlais
-rm -Rf  ../trainingPack/src/environments/Tetrlais/*
-cp -R src/* ../trainingPack/src/environments/Tetrlais/
+rm  ../techDist/src/environments/Tetrlais/*.class
+rm  ../techDist/src/environments/Tetrlais/Tetrlais/*.class
 
-echo " * Building the Tetris jar and moving it to the trainingPack to the training pack"
+cp src/*.java ../techDist/src/environments/Tetrlais/
+cp src/Tetrlais/*.java ../techDist/src/environments/Tetrlais/Tetrlais/
+
+echo " * Building the Tetris jar and moving it to the techDist to the training pack"
 cd bin
-rm ../../trainingPack/bin/environments/Tetrlais.jar
-jar -cf ../../trainingPack/bin/environments/Tetrlais.jar ./Tetrlais/*.class
+rm ../../techDist/bin/environments/Tetrlais.jar
+jar -cf ../../techDist/bin/environments/Tetrlais.jar ./Tetrlais/*.class
 cd ../../
+
+#################
+## Helicopter
+#################
+echo " * Copying Helicopter source code to the training pack"
+##Copy the source
+cd Helicopter
+rm  ../techDist/src/environments/Helicopter/*.java
+rm  ../techDist/src/environments/Helicopter/Helicopter/*.java
+cp src/*.java ../techDist/src/environments/Helicopter/
+cp src/Helicopter/*.java ../techDist/src/environments/Helicopter/Helicopter/
+
+echo " * Building the Helicopter jar and moving it to the techDist to the training pack"
+cd bin
+rm ../../techDist/bin/environments/Helicopter.jar
+jar -cf ../../techDist/bin/environments/Helicopter.jar ./Helicopter/*.class
+cd ../../
+
 
 
 #################
@@ -51,20 +76,27 @@ cd ../../
 #################
 #Copy source and binaries for GenericSarsaLambda
 echo " * Copying to source and binaries for GenericSarsaLamda"
-rm -Rf trainingPack/src/agents/GenericSarsaLambdaJava/*
-rm -Rf trainingPack/bin/agents/GenericSarsaLambdaJava/*
-cp -R GenericSarsaLambda/src/* trainingPack/src/agents/GenericSarsaLambdaJava/
-cp -R GenericSarsaLambda/bin/* trainingPack/bin/agents/GenericSarsaLambdaJava/
+rm -f techDist/src/agents/GenericSarsaLambdaJava/*.java
+rm -f techDist/src/agents/GenericSarsaLambdaJava/GenericSarsaLambda/*.java
+
+rm -f techDist/bin/agents/GenericSarsaLambdaJava/*.class
+rm -f techDist/bin/agents/GenericSarsaLambdaJava/GenericSarsaLambda/*.class
+
+cp GenericSarsaLambda/src/*.java techDist/src/agents/GenericSarsaLambdaJava/
+cp GenericSarsaLambda/src/GenericSarsaLambda/*.java techDist/src/agents/GenericSarsaLambdaJava/GenericSarsaLambda/
+
+cp GenericSarsaLambda/bin/*.class techDist/bin/agents/GenericSarsaLambdaJava/
+cp GenericSarsaLambda/bin/GenericSarsaLambda/*.class techDist/bin/agents/GenericSarsaLambdaJava/GenericSarsaLambda/
 
 #################
 ## Java Trainer
 #################
 echo " * Copying to source and binaries for Java Trainer"
-rm -Rf trainingPack/src/trainingExperiments/trainJava/*
-rm -Rf trainingPack/bin/trainingExperiments/trainJava/*
-cp -R JavaTrainer/src/* trainingPack/src/trainingExperiments/trainJava/
-cp -R JavaTrainer/bin/* trainingPack/bin/trainingExperiments/trainJava/
-cp scripts/runTrainer.bash trainingPack/
-cp scripts/runViz.bash trainingPack/
+rm techDist/src/trainingExperiments/trainJava/*.java
+rm techDist/bin/trainingExperiments/trainJava/*.class
+
+cp JavaTrainer/src/*.java techDist/src/trainingExperiments/trainJava/
+cp JavaTrainer/bin/*.class techDist/bin/trainingExperiments/trainJava/
+
 
 
