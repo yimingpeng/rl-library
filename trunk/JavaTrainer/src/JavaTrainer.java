ad97d9b8f6e9a56963f00e0cd729af603d5eed1a
bttanner <bttanner@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-08-13 20:55:59 +0000

    few more changes to java Trainer

--- trunk/JavaTrainer/src/JavaTrainer.java
+++ trunk/JavaTrainer/src/JavaTrainer.java
@@ -46,14 +46,18 @@ public class JavaTrainer {
 		//of episodes run. You have 100 000 steps per episode to terminate in, other wise 
 		// the glue terminates the episode
 		int sum=0;
-		for(int i=0;i<1000000;i++){
+		for(int i=0;i<10000;i++){
 			RLGlue.RL_episode(100000);
 			sum+=RLGlue.RL_num_steps();
-			if(i%50==0){
-				System.out.println("Running episode: "+i+" total steps in last bunch is: "+sum);
+			if((i+1)%50==0){
+				System.out.println("Running episode: "+(i+1)+" total steps in last bunch is: "+sum);
 				sum=0;
 			}
 		}
+		
+		//This program will pop up the visualizer to take a look at what's happening
+		//We need to use something caled TinyGlue to manage the RL-Glue calls for us when the visualizer is running
+
 		TinyGlue theTinyGlue= new TinyGlue();
 		//Set this otherwise the first step of theTinyGlue will call RL_init and undo all our learning
 		theTinyGlue.setInited(true);
@@ -71,18 +75,25 @@ public class JavaTrainer {
 			Thread.sleep(10);
 		}
 
-		//finishes off the episode you were in when you ran out of steps
+		//finishes off the episode you were in when you ran out of steps.  This is important so we can go back to regular RL-Glue calls
+		//without the Tiny Glue
 		while(theTinyGlue.step());
 
 
+		
 		//Stop visualizing the agent's actions
 		System.out.println("out of the display loop");
 		theViz.stopVisualizing();
 
-		//Run a few more steps without the visualizer
-		System.out.println("running 1000 steps quietly");
-		for(int i=0;i<1000;i++){
-			theTinyGlue.step();
+		
+		sum=0;
+		for(int i=0;i<100;i++){
+			RLGlue.RL_episode(100000);
+			sum+=RLGlue.RL_num_steps();
+			if((i+1)%50==0){
+				System.out.println("Running episode: "+(i+1)+" total steps in last bunch is: "+sum);
+				sum=0;
+			}
 		}
 
 		//Run a few more steps with the visualizer on again
