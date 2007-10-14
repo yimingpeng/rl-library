bfd63a77073641465b76c527ec2779284c752735
markplee <markplee@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-10-14 18:43:38 +0000

    Python trainer and updated java task spec parser

---
+++ trunk/rl-competition/trainers/consoleTrainerPython/src/consoleTrainer.py
@@ -0,0 +1,57 @@
+# console Trainer for RL Competition
+# Copyright (C) 2007, Brian Tanner brian@tannerpages.com (http://brian.tannerpages.com/)
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+from RL_experiment import *
+from consoleTrainerHelper import *
+
+def main():
+	# Basically you want to do: (num is the problem number)
+	# 	loadTetris(num) OR
+	# 	loadMountainCar(num) OR
+	# 	loadHelicopter()
+	#
+	# and then,
+	#		runExperiment()
+	# runMountainCarExperiment does this for each of the 9 versions of mountain car
+	runMountainCarExperiment()
+
+
+# A sample trainer to run through the different mountain car training values
+def runMountainCarExperiment():
+	for paramSet in range(10):
+		loadMountainCar(paramSet)
+		runExperiment()
+
+# Run a certain number of episodes and return the total number of steps
+def runEpisodes(episodeCount, maxEpisodeLength):
+	totalSteps = 0
+	for i in range(episodeCount):
+		RL_episode(maxEpisodeLength)
+		totalSteps += RL_num_steps()
+	return totalSteps
+
+# Runs 10 episodes and reports the average number of steps for each
+def runExperiment():
+	RL_init()
+	episodesToRun = 10
+	totalSteps = runEpisodes(episodesToRun,1000)
+	averageSteps = float(totalSteps)/float(episodesToRun)
+	print "Average steps per episode: %.2f\n" % (averageSteps)
+	RL_cleanup()
+
+if __name__ == "__main__":
+	main()
