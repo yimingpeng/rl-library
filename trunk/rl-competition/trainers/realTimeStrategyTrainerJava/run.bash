1623065e35302ae99df058f9365613c528269a43
bttanner <bttanner@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-10-14 16:20:38 +0000

    Made the RTS trainer more like ours

--- trunk/rl-competition/trainers/realTimeStrategyTrainerJava/run.bash
+++ trunk/rl-competition/trainers/realTimeStrategyTrainerJava/run.bash
@@ -1,3 +1,32 @@
-#!/bin/sh
-java -cp ../../RL-Glue/RL-Glue/Java/RL-Glue.jar:. OLExperiment
+#/bin/bash
+
+
+#Variables
+basePath=../..
+libPath=$basePath/libraries
+
+compLib=$libPath/RLVizLib.jar
+
+glueExe=$libPath/RL_glue
+rtsEXE=$basePath/domains/realTimeStrategy/bin/rlgenv
+
+$glueExe &
+gluePID=$!
+echo "Starting up RL-glue - PID=$gluePID"
+
+$rtsEXE &
+rtsPID=$!
+echo "Starting up real time strategy game - PID=$rtsPID"
+
+java -Xmx128M -cp $compLib:./bin/ OLExperiment
+
+echo "-- Console Real Time Strategy Trainer finished"
+
+echo "-- Waiting for the real time strategy game to die..."
+wait $rtsPID
+echo "   + real time strategy game terminated"
+echo "-- Waiting for the Glue to die..."
+wait $gluePID
+echo "   + Glue terminated"
+
 
