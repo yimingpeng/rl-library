3575d5ca8371f20fb73912b9462588d4943e6591
brian <brian@tannerpages.com>
2007-10-27 15:19:32 +0000

    updated random agent

--- rl-competition/agents/randomAgentJava/src/RandomAgent/RandomAgent.java
+++ rl-competition/agents/randomAgentJava/src/RandomAgent/RandomAgent.java
@@ -38,33 +38,28 @@ public class RandomAgent implements Agent {
         }
 
 	public void agent_cleanup() {
-		System.out.println("\t -- agent_cleanup called in RandomAgent");
 	}
 
 	public void agent_end(double arg0) {
-            // TODO Auto-generated method stub
-		
+
 	}
 
 	public void agent_freeze() {
-            // TODO Auto-generated method stub
-		
+
 	}
 
 	public void agent_init(String taskSpec) {
             TSO = new TaskSpecObject(taskSpec);
-            // TODO Auto-generated method stub
+
             action = new Action(TSO.num_discrete_action_dims,TSO.num_continuous_action_dims);	
 	}
 
 	public String agent_message(String arg0) {
-            // TODO Auto-generated method stub
             return null;
 	}
 
 	public Action agent_start(Observation o) {
             randomify(action);
-            //ask(o,action);
             return action;
 	}
 
@@ -73,41 +68,15 @@ public class RandomAgent implements Agent {
             return action;
 	}
 
-	public double getValueForState(Observation theObservation) {
-            // TODO Auto-generated method stub
-            return 0;
-	}
 	private void randomify(Action action){
             for(int i=0;i<TSO.num_discrete_action_dims;i++){
                 action.intArray[i]=random.nextInt(((int)TSO.action_maxs[i]+1)-(int)TSO.action_mins[i]) + ((int)TSO.action_mins[i]);
-                // System.out.println(action.intArray[i]);
             }
             for(int i=0;i<TSO.num_continuous_action_dims;i++){
                 action.doubleArray[i]=random.nextDouble()*(TSO.action_maxs[i] - TSO.action_mins[i]) + TSO.action_mins[i];
-                //System.out.println(action.doubleArray[i]);
             }
        	}
 	
 
-      /*  
-        public static void main(String [] args) throws Exception
-	{
-	    
-		String taskSpec = "2:e:2_[f,f]_[-1.2,0.6]_[-0.07,0.07]:1_[f]_[-5,5]";
-		
-		TaskSpecObject taskObject = new TaskSpecObject(taskSpec);
-		System.err.println(taskObject);
-                
-                TaskSpecObject specObj = new TaskSpecObject(taskSpec);
-		// TODO Auto-generated method stub
-		Action theaction = new Action(specObj.num_discrete_action_dims,specObj.num_continuous_action_dims);
-                RandomAgent ra = new RandomAgent();
-                ra.agent_init(taskSpec);
-                Observation o = new Observation(1,1);
-                for(int i=0; i< 50; i++){
-                    ra.agent_step(i, o);
-                }
-                
-			
-	}*/
+
 }
--- rl-competition/runDemo.bash
+++ rl-competition/runDemo.bash
@@ -2,7 +2,7 @@
 
 
 #Variables
-basePath=../..
+basePath=./
 systemPath=$basePath/system
 libPath=$systemPath/libraries
 
@@ -12,23 +12,46 @@ glueExe=$systemPath/RL_glue
 guiLib=$libPath/forms-1.1.0.jar
 envShellLib=$libPath/EnvironmentShell.jar
 
+agentPath=agents/randomAgentJava
+demoPath=trainers/guiTrainerJava
+
+
 RLVIZ_LIB_PATH=$PWD/$libPath
 
+#
+#Start the RL_glue program  in the background and get it's process id
+#
 $glueExe &
 gluePID=$!
 echo "Starting up RL-glue - PID=$gluePID"
 
+#
+#Start the random agent in the background and get it's process id
+#
+java -Xmx128M -cp $compLib:$agentPath/bin rlglue.agent.AgentLoader RandomAgent.RandomAgent &
+agentPID=$!
+
+
+#
+#Start the environment in the background and get it's process id
+#
 java -Xmx128M -DRLVIZ_LIB_PATH=$RLVIZ_LIB_PATH -cp $compLib:$envShellLib rlglue.environment.EnvironmentLoader environmentShell.EnvironmentShell &
 envShellPID=$!
 echo "Starting up dynamic environment loader - PID=$envShellPID"
 
-java -Xmx128M -DRLVIZ_LIB_PATH=$RLVIZ_LIB_PATH -cp $compLib:$guiLib:./bin/rlViz.jar btViz.GraphicalDriver
+#
+#Start the demo visualizer program
+#
+java -Xmx128M -DRLVIZ_LIB_PATH=$RLVIZ_LIB_PATH -cp $compLib:$guiLib:$demoPath/bin/rlViz.jar btViz.GraphicalDriver
 
 echo "-- Visualizer is finished"
 
 echo "-- Waiting for the Environment to die..."
 wait $envShellPID
 echo "   + Environment terminated"
+echo "-- Waiting for the Agent to die..."
+wait $agentPID
+echo "   + Agent terminated"
 echo "-- Waiting for the Glue to die..."
 wait $gluePID
 echo "   + Glue terminated"
