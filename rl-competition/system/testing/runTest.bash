86b30f3eaa5f22faca5c900dd08822d50bdc650f
radkie <radkie@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-10-29 23:14:24 +0000

    fixed the testing harness

--- rl-competition/system/testing/runTest.bash
+++ rl-competition/system/testing/runTest.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ../
+cd ../..
 echo "Making..."
 make
 
@@ -14,84 +14,100 @@ export ENV=mountaincar
 export AGENT=randomAgentJava
 echo "load $Agent"
 cd agents/$AGENT/
+make
 ./run.bash &
 
-export TRAINER=consoleTrainerJava
+export TRAINER=system/testing/consoleTrainerTesting
 echo "Testing $AGENT on $TRAINER with env $ENV"
-cd ../../trainers/$TRAINER
+cd ../../$TRAINER
+make
 ./run.bash $ENV
 
 #check java random agent with java console trainer on Heli
 export AGENT=randomAgentJava
 echo "load $Agent"
-cd ../../agents/$AGENT/
+cd ../../../agents/$AGENT/
+make
 ./run.bash &
 
 export ENV=helicopter
-export TRAINER=consoleTrainerJava
+export TRAINER=system/testing/consoleTrainerTesting
 echo "Testing $AGENT on $TRAINER with env $ENV"
-cd ../../trainers/$TRAINER
+cd ../../$TRAINER
+make
 ./run.bash $ENV
 
 #check java random agent with java console trainer on Tetris
 export AGENT=randomAgentJava
 echo "load $Agent"
-cd ../../agents/$AGENT/
+cd ../../../agents/$AGENT/
+make
 ./run.bash &
 
 export ENV=tetris
-export TRAINER=consoleTrainerJava
+export TRAINER=system/testing/consoleTrainerTesting
 echo "Testing $AGENT on $TRAINER with env $ENV"
-cd ../../trainers/$TRAINER
+cd ../../$TRAINER
+make
 ./run.bash $ENV
 
 #check java tetris agent with java console trainer on Tetris
 export AGENT=tetrisAgentJava
 echo "load $Agent"
-cd ../../agents/$AGENT/
+cd ../../../agents/$AGENT/
+make
 ./run.bash &
 
-export TRAINER=consoleTrainerJava
+export TRAINER=system/testing/consoleTrainerTesting
 echo "Testing $AGENT on $TRAINER with env $ENV"
-cd ../../trainers/$TRAINER
+cd ../../$TRAINER
+make
 ./run.bash $ENV
 
 killall RL_glue
 #check mountaincar with java console trainer
 export AGENT=mountainCarAgentCPP
 echo "load $Agent"
-cd ../../agents/$AGENT/
+cd ../../../agents/$AGENT/
+make
 ./run.bash &
 
 echo "Testing $AGENT on $TRAINER"
-cd ../../trainers/$TRAINER
+cd ../../$TRAINER
+make
 ./run.bash
 
 #check mountaincar CPP agent with console CPP trainer 
 echo "load $Agent"
-cd ../../agents/$AGENT/
+cd ../../../agents/$AGENT/
+make
 ./run.bash &
 
 export TRAINER=consoleTrainerCPP
 echo "Testing $AGENT on $TRAINER"
 cd ../../trainers/$TRAINER
+make
 ./run.bash
 
 #check random java agent with CPP console trainer 
 export AGENT=randomAgentJava
 echo "load $Agent"
 cd ../../agents/$AGENT/
+make
 ./run.bash &
 
 echo "Testing $AGENT on $TRAINER"
 cd ../../trainers/$TRAINER
+make
 ./run.bash
 
 #check random java agent with java gui trainer
 cd ../../agents/$AGENT/
+make
 ./run.bash &
 
 export TRAINER=guiTrainerJava
 echo "Testing $AGENT on $TRAINER"
 cd ../../trainers/$TRAINER
+make
 ./run.bash
