a984b87290deadb9d6b9e758ec3874a9d2c8e1b8
whitienator <whitienator@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-10-31 18:07:56 +0000

    removed agents/tetrisAgentJava

--- rl-competition/agents/tetrisAgentJava/run.bash
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-#Variables
-compLib=../../system/libraries/RLVizLib.jar
-
-
-java -Xmx128M -cp $compLib:./bin rlglue.agent.AgentLoader TetrisAgent.TetrisAgent
-
-echo "-- Agent was killed"
