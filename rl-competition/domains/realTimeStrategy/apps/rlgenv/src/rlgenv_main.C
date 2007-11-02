ca3976a2a009d10b95f1ad91f2c4375f61eb912c
brian <brian@tannerpages.com>
2007-11-02 17:52:32 +0000

    Small fixes to the source of rlglue_env.H

--- rl-competition/domains/realTimeStrategy/apps/rlgenv/src/rlgenv_main.C
+++ rl-competition/domains/realTimeStrategy/apps/rlgenv/src/rlgenv_main.C
@@ -29,16 +29,17 @@
 #include <Network/RL_network.h>
 
 /* Provide forward declaration of environment interface */
-extern Task_specification env_init();
-extern Observation env_start();
-extern Reward_observation env_step(Action a);
-extern void env_cleanup();
-extern void env_set_state(State_key sk);
-extern void env_set_random_seed(Random_seed_key rsk);
-extern State_key env_get_state();
-extern Random_seed_key env_get_random_seed();
-extern Message env_message(const Message inMessage);
-
+extern "C"{
+	Task_specification env_init();
+ Observation env_start();
+ Reward_observation env_step(Action a);
+ void env_cleanup();
+ void env_set_state(State_key sk);
+ void env_set_random_seed(Random_seed_key rsk);
+ State_key env_get_state();
+ Random_seed_key env_get_random_seed();
+ Message env_message(const Message inMessage);
+}
 static const char* kUnknownMessage = "Unknown Message: %s\n";
 
 static Action theAction                 = {0};
