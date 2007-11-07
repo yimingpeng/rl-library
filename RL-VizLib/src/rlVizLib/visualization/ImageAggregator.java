ab5cfd0419c19866ee18bd30c4a7bfafff709b98
brian <brian@tannerpages.com>
2007-11-07 01:09:30 +0000

    making the Build 530 release tag

--- RL-VizLib/src/rlVizLib/visualization/ImageAggregator.java
+++
@@ -1,7 +0,0 @@
-package rlVizLib.visualization;
-
-//This interface means that you follow the observer pattern and care when stuff happens
-public interface ImageAggregator {
-	public void receiveNotification();
-
-}
--- techDist/rl-glue/Examples/Network_Java/bin/runExample.bash
+++
@@ -1,4 +0,0 @@
-./RL_Glue &
-java -cp ../../../RL-Glue/Java/RL-Glue.jar:./ rlglue.agent.AgentLoader SarsaAgent &
-java -cp ../../../RL-Glue/Java/RL-Glue.jar:./ rlglue.environment.EnvironmentLoader Mines &
-java -cp ../../../RL-Glue/Java/RL-Glue.jar:./ Experiment
