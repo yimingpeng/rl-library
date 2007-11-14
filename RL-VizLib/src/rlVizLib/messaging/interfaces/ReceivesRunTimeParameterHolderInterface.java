63f3f0c9ffda3bd1112ab575f48e5e45cf499c36
markplee <markplee@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-11-14 22:34:49 +0000

    Creating RC3 Pre tag

--- RL-VizLib/src/rlVizLib/messaging/interfaces/ReceivesRunTimeParameterHolderInterface.java
+++
@@ -1,7 +0,0 @@
-package rlVizLib.messaging.interfaces;
-
-import rlVizLib.general.ParameterHolder;
-
-public interface ReceivesRunTimeParameterHolderInterface {
-	public boolean receiveRunTimeParameters(ParameterHolder theParams);
-}
--- techDist/src/trainingExperiments/trainJava/Experiment.makefile
+++
@@ -1,6 +0,0 @@
-JCC = javac
-BIN = ../../../bin
-CLASSPATH = $(BIN)/RL-Train.jar
-
-Experiment:
-	$(JCC) -cp $(CLASSPATH) *.java -d $(BIN)/trainingExperiments/trainJava/
