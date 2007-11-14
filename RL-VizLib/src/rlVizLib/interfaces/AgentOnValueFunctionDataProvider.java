fd8da38f8c4329ca4bddecac759f272c3f853be5
markplee <markplee@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-11-14 22:40:17 +0000

    Creating B540 release tag

--- RL-VizLib/src/rlVizLib/interfaces/AgentOnValueFunctionDataProvider.java
+++
@@ -1,9 +0,0 @@
-package rlVizLib.interfaces;
-
-
-public interface AgentOnValueFunctionDataProvider {
-	public double getCurrentStateInDimension(int whichDimension);
-	public double getMinValueForDim(int whichDimension);
-	public double getMaxValueForDim(int whichDimension);
-	public void updateAgentState();
-}
--- techDist/src/environments/mountainCar/MountainCar/MountainCar.makefile
+++
@@ -1,9 +0,0 @@
-# Makefile for compiling agents, enviroment and experiments
-BIN = ../../../../bin
-CLASSPATH = $(BIN)/RL-Train.jar
-JCC = javac
-
-MountainCar:
-	$(JCC) -cp $(CLASSPATH) *.java
-	cd .. && jar cvf ../../../bin/environments/MountainCar.jar ./MountainCar/*.class
-	rm -f *.class
