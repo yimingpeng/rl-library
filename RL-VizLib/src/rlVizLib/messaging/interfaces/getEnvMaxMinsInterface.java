f81d0f6bad572a1970016ed363bca10e10b56f7e
brian <brian@tannerpages.com>
2007-11-16 03:22:45 +0000

    Creating the final RC3 tag

--- RL-VizLib/src/rlVizLib/messaging/interfaces/getEnvMaxMinsInterface.java
+++
@@ -1,8 +0,0 @@
-package rlVizLib.messaging.interfaces;
-
-public interface getEnvMaxMinsInterface {
-	public int getNumVars();
-	public double getMinValueForQuerableVariable(int dimension);
-	public double getMaxValueForQuerableVariable(int dimension);
-
-}
