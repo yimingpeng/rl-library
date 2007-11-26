ca5adefcd870f48cb6a54d7eeea9e042edb576ef
brian <brian@tannerpages.com>
2007-11-26 07:56:52 +0000

    Updated RL-Library, it all works

--- scripts/runNet.bash
+++ scripts/runNet.bash
@@ -1,12 +1,12 @@
 #/bin/bash
-glueRoot=../rl-glue
+glueRoot=../system/rl-glue
 glueExe=$glueRoot/RL-Glue/bin/RL_glue
 
-RLVizDir=../libs
+RLVizDir=../system/libs
 RLVizJar=$RLVizDir/RLVizApp.jar
 
 
 $glueExe &
-java -Xmx128M -DRLVIZ_LIB_PATH=../dist -classpath $RLVizJar btViz.NetGraphicalDriverBothDynamic &
-java -Xmx128M -DRLVIZ_LIB_PATH=../dist -classpath $RLVizJar rlglue.agent.AgentLoader agentShell.AgentShell &
-java -Xmx128M -DRLVIZ_LIB_PATH=../dist -classpath $RLVizJar rlglue.environment.EnvironmentLoader environmentShell.EnvironmentShell 
+java -Xmx128M -DRLVIZ_LIB_PATH=../system/dist -classpath $RLVizJar btViz.NetGraphicalDriverBothDynamic &
+java -Xmx128M -DRLVIZ_LIB_PATH=../system/dist -classpath $RLVizJar rlglue.agent.AgentLoader agentShell.AgentShell &
+java -Xmx128M -DRLVIZ_LIB_PATH=../system/dist -classpath $RLVizJar rlglue.environment.EnvironmentLoader environmentShell.EnvironmentShell 
