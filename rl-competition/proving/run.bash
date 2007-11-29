0eb4cb446cf4b38b649eaf6517fb2dfcd4ffb33a
brian <brian@tannerpages.com>
2007-11-29 07:02:08 +0000

    Latest jars

--- rl-competition/proving/run.bash
+++ rl-competition/proving/run.bash
@@ -14,26 +14,26 @@ envShellLib=$libPath/EnvironmentShell.jar
 
 
 
-glueExe=$systemPath/RL_glue
+# glueExe=$systemPath/RL_glue
+# 
+# $glueExe &
+# gluePID=$!
+# echo "Starting up RL-glue - PID=$gluePID"
+# 
+#java -DRLVIZ_LIB_PATH=$PWD/$provingPath -Xmx128M -cp $compLib:$envShellLib rlglue.environment.EnvironmentLoader environmentShell.EnvironmentShell  &
+#envShellPID=$!
 
-$glueExe &
-gluePID=$!
-echo "Starting up RL-glue - PID=$gluePID"
-
-java -DRLVIZ_LIB_PATH=$PWD/$provingPath -Xmx128M -cp $compLib:$envShellLib rlglue.environment.EnvironmentLoader environmentShell.EnvironmentShell  &
-envShellPID=$!
-
-echo "Starting up dynamic environment loader - PID=$envShellPID"
+#echo "Starting up dynamic environment loader - PID=$envShellPID"
 
 java -Xmx128M -cp $compLib -jar ./bin/Proving.jar
 
 echo "-- Console Trainer finished"
 
-echo "-- Waiting for the Environment to die..."
-wait $envShellPID
-echo "   + Environment terminated"
-echo "-- Waiting for the Glue to die..."
-wait $gluePID
-echo "   + Glue terminated"
-
+#echo "-- Waiting for the Environment to die..."
+#wait $envShellPID
+#echo "   + Environment terminated"
+# echo "-- Waiting for the Glue to die..."
+# wait $gluePID
+# echo "   + Glue terminated"
+# 
 
