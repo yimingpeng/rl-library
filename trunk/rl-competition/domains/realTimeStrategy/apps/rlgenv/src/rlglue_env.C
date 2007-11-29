4aa8e90672acc5ad8d107d96e9e601ad845770e8
marc.lanctot <marc.lanctot@b17b758d-1535-0410-a358-a1ba4bb9075e>
2007-11-29 07:58:41 +0000

    added loading of parms from a file, and skeleton for bot2

--- trunk/rl-competition/domains/realTimeStrategy/apps/rlgenv/src/rlglue_env.C
+++ trunk/rl-competition/domains/realTimeStrategy/apps/rlgenv/src/rlglue_env.C
@@ -31,7 +31,7 @@ static bool debug = false;
 // GUI vars. To enable SDL GUI, set use_gui to true 
 // *and* ENABLE_GUI=1 in Makefile
 static SDL_GUI<MiniGameState> gui;
-static bool use_gui = false;
+static bool use_gui = true;
 static int gui_delay = 25;
 static std::map<std::string, SDL_GUI<MiniGameState>::Marker> markers;
 
@@ -84,6 +84,7 @@ static void init(ParameterHolder * phPtr = NULL)
   
   parms = new MiniGameParameters;
   if (phPtr != NULL) copy_parms(phPtr, parms);
+  //else load_parms("/home/lanctot/rlgparms2.txt", parms);
   
   //opponent = new RLComp08Bot1(0);
   opponent = get_opponent(parms);
--- trunk/rl-competition/domains/realTimeStrategy/apps/rlgexp/src/rlgexp_main.C
+++ trunk/rl-competition/domains/realTimeStrategy/apps/rlgexp/src/rlgexp_main.C
@@ -5,7 +5,7 @@
 
 using namespace std; 
 
-#define NUM_EPISODES 10
+#define NUM_EPISODES 1
 
 //int rl_num_steps[NUM_EPISODES];
 //double rl_return[NUM_EPISODES];
