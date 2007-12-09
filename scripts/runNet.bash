3a3781964bb70d28b52bfed3fd91918ef3aea66d
brian <brian@tannerpages.com>
2007-12-09 20:59:24 +0000

    updated script to run

--- scripts/runNet.bash
+++ scripts/runNet.bash
@@ -2,11 +2,14 @@
 glueRoot=../system/rl-glue
 glueExe=$glueRoot/RL-Glue/bin/RL_glue
 
-RLVizDir=../system/libs
+RLVizDir=../system/libs/rl-viz
 RLVizJar=$RLVizDir/RLVizApp.jar
+RLVizLibJar=$RLVizDir/RLVizLib.jar
+EnvShellJar=$RLVizDir/EnvironmentShell.jar
+AgentShellJar=$RLVizDir/AgentShell.jar
 
 
 $glueExe &
-java -Xmx128M -DRLVIZ_LIB_PATH=../system/dist -classpath $RLVizJar btViz.NetGraphicalDriverBothDynamic &
-java -Xmx128M -DRLVIZ_LIB_PATH=../system/dist -classpath $RLVizJar rlglue.agent.AgentLoader agentShell.AgentShell &
-java -Xmx128M -DRLVIZ_LIB_PATH=../system/dist -classpath $RLVizJar rlglue.environment.EnvironmentLoader environmentShell.EnvironmentShell 
+java -Xmx128M -DRLVIZ_LIB_PATH=../system/dist -classpath $RLVizJar:$RLVizLibJar btViz.NetGraphicalDriverBothDynamic &
+java -Xmx128M -DRLVIZ_LIB_PATH=../system/dist -classpath $RLVizLibJar:$AgentShellJar rlglue.agent.AgentLoader agentShell.AgentShell &
+java -Xmx128M -DRLVIZ_LIB_PATH=../system/dist -classpath $RLVizLibJar:$EnvShellJar rlglue.environment.EnvironmentLoader environmentShell.EnvironmentShell 
