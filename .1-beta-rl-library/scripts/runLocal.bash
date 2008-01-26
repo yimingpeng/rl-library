6f7106a5edde8287a65c8d79bb93895484e63e75
brian <brian@tannerpages.com>
2008-01-26 20:08:47 +0000

    killing accidental tag

--- .1-beta-rl-library/scripts/runLocal.bash
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-RLVizPath=../system/libs/rl-viz
-AgentEnvPath=../system/dist
-java -Xmx1024M  -DRLVIZ_LIB_PATH=$AgentEnvPath -classpath $RLVizPath/RLVizApp.jar:$RLVizPath/RLVizLib.jar:$RLVizPath/EnvironmentShell.jar:$RLVizPath/AgentShell.jar btViz.LocalGraphicalDriver
