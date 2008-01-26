574da0864a2068368b0b54791dce1f4a279db2dd
brian@tannerpages.com <brian@tannerpages.com@b17b758d-1535-0410-a358-a1ba4bb9075e>
2008-01-26 21:37:58 +0000

    updated trainers in scripts to use the system/scripts/rl-library-includes.sh script

--- trunk/scripts/runLocal.bash
+++ trunk/scripts/runLocal.bash
@@ -1,4 +1,14 @@
 #/bin/bash
-RLVizPath=../system/libs/rl-viz
-AgentEnvPath=../system/dist
-java -Xmx1024M  -DRLVIZ_LIB_PATH=$AgentEnvPath -classpath $RLVizPath/RLVizApp.jar:$RLVizPath/RLVizLib.jar:$RLVizPath/EnvironmentShell.jar:$RLVizPath/AgentShell.jar btViz.LocalGraphicalDriver
+basePath=../
+systemPath=$basePath/system
+
+#Source a script that sets all important functions and variables
+source $systemPath/scripts/rl-library-includes.sh
+
+
+startLocalGuiTrainer
+
+
+# RLVizPath=../system/libs/rl-viz
+# AgentEnvPath=../system/dist
+# java -Xmx1024M  -DRLVIZ_LIB_PATH=$AgentEnvPath -classpath $RLVizPath/RLVizApp.jar:$RLVizPath/RLVizLib.jar:$RLVizPath/EnvironmentShell.jar:$RLVizPath/AgentShell.jar btViz.LocalGraphicalDriver
