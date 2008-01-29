089b445bbc8b46b4eb4e770a6df15d1546962d14
brian@tannerpages.com <brian@tannerpages.com@b17b758d-1535-0410-a358-a1ba4bb9075e>
2008-01-29 23:10:06 +0000

    moving the run scripts for the graphical experiment into projects/experiments/guiExperiment

---
+++ trunk/projects/experiments/guiExperiment/runNet.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+basePath=../../..
+systemPath=$basePath/system
+
+#Source a script that sets all important functions and variables
+source $systemPath/scripts/rl-library-includes.sh
+
+startRLGlueInBackGround
+startEnvShellInBackGround
+startAgentShellInBackGround
+startNetGuiTrainer
+
+waitForAgentShellToDie
+waitForEnvShellToDie
+waitForRLGlueToDie
+
