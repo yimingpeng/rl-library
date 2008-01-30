fa1ee51bb48fb5214352548fa75a382627c98c0a
brian@tannerpages.com <brian@tannerpages.com@b17b758d-1535-0410-a358-a1ba4bb9075e>
2008-01-30 04:24:24 +0000

    removing the stable tag

--- tags/versions/stable/projects/agents/randomAgentPython/run.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-basePath=../../..
-systemPath=$basePath/system
-rlGluePath=$systemPath/rl-glue/RL-Glue
-PYTHONPATH=$rlGluePath/Python:./src python -c "import rlglue.agent.AgentLoader" RandomAgent
