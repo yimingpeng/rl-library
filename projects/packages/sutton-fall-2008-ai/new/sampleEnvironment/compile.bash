c2a9858c0ec06d75962d8e8262fbc12e1a7e842e
brian <brian@tannerpages.com>
2008-10-28 00:20:25 +0000

    Things are screwed up.

---
+++ projects/packages/sutton-fall-2008-ai/new/sampleEnvironment/compile.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+basePath=..
+systemPath=$basePath/system
+
+RLVizLibPath=$systemPath/libs/rl-viz/RLVizLib.jar
+
+
+rm -f src/*.class
+javac -classpath $RLVizLibPath src/*.java
+cd src
+jar cmf MANIFEST.MF $basePath/../JARS/sampleEnvironment.jar *.class
