d26426b7120d99a0c6620bfff99b2848eeace310
brian@tannerpages.com <brian@tannerpages.com@b17b758d-1535-0410-a358-a1ba4bb9075e>
2008-12-03 19:11:16 +0000

    Updated teh critter controller to have reasonable strength of motor commands and fixed flipped f/b and l/r controls.

--- trunk/projects/agents/keyboardAgentJava/src/org/rlcommunity/agents/keyboard/mappings/ExpandedCritterControlSettings.java
+++ trunk/projects/agents/keyboardAgentJava/src/org/rlcommunity/agents/keyboard/mappings/ExpandedCritterControlSettings.java
@@ -3,13 +3,13 @@
  * http://bt-recordbook.googlecode.com/
  * brian@tannerpages.com
  * http://brian.tannerpages.com
- * 
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,7 +29,8 @@ import org.rlcommunity.rlglue.codec.taskspec.TaskSpec;
  *
  * @author Brian Tanner
  */
-public class ExpandedCritterControlSettings{
+public class ExpandedCritterControlSettings {
+
     public static void ensureTaskSpecMatchesExpectation(TaskSpec TSO) {
         assert (TSO.getNumDiscreteActionDims() == 4);
         assert (TSO.getNumContinuousActionDims() == 0);
@@ -37,19 +38,25 @@ public class ExpandedCritterControlSettings{
 //        assert (TSO.getDiscreteActionRange(0).getMax() == 1);
     }
 
-    public static void addActions(JComponent thePanel, IntActionReceiver theTarget) {
-        javax.swing.Action Forward=new SimpleIntAction("Forward",new int[]{1,50,0,0}, theTarget);
-        javax.swing.Action Back=new SimpleIntAction("Back",new int[]{1,-50,0,0}, theTarget);
-        javax.swing.Action RotateLeft=new SimpleIntAction("Rotate Left",new int[]{1, 0, 0,20}, theTarget);
-        javax.swing.Action RotateRight=new SimpleIntAction("Rotate Right",new int[]{1, 0, 0,-20}, theTarget);
-        
-        thePanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0), "Back");
+    public static void addActions(JComponent thePanel,
+                                  IntActionReceiver theTarget) {
+        javax.swing.Action Forward = new SimpleIntAction("Forward", new int[]{1, 20, 0, 0}, theTarget);
+        javax.swing.Action Back = new SimpleIntAction("Back", new int[]{1, -20, 0, 0}, theTarget);
+        javax.swing.Action RotateLeft = new SimpleIntAction("Rotate Left",new int[]{1, 0, 0,-10}, theTarget);
+
+        javax.swing.Action RotateRight = new SimpleIntAction("Rotate Right",new int[]{1, 0, 0,10}, theTarget);
+
+        thePanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN,
+                0), "Back");
         thePanel.getActionMap().put("Back", Back);
-        thePanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), "Forward");
+        thePanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_UP,
+                0), "Forward");
         thePanel.getActionMap().put("Forward", Forward);
-        thePanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), "RotateLeft");
+        thePanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT,
+                0), "RotateLeft");
         thePanel.getActionMap().put("RotateLeft", RotateLeft);
-        thePanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), "RotateRight");
+        thePanel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT,
+                0), "RotateRight");
         thePanel.getActionMap().put("RotateRight", RotateRight);
 
 
@@ -58,7 +65,4 @@ public class ExpandedCritterControlSettings{
         thePanel.add(new JButton(RotateLeft));
         thePanel.add(new JButton(RotateRight));
     }
-    
-    
 }
-
