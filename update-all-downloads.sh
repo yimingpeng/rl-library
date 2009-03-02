20dd1534d509255bb84f21ea63256cfa05840b4c
brian <brian@tannerpages.com>
2009-03-02 02:09:10 +0000

    Created a script to update all the downloads.

---
+++ update-all-downloads.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+###
+#Environments
+###
+
+pushd projects/environments/mountainCar
+svn up
+bash makeDistribution.bash
+popd
+
+pushd projects/environments/acrobot
+svn up
+bash makeDistribution.bash
+popd
+
+pushd projects/environments/helicopter
+svn up
+bash makeDistribution.bash
+popd
+
+pushd projects/environments/cartpole
+svn up
+bash makeDistribution.bash
+popd
+
+pushd projects/environments/tetris
+svn up
+bash makeDistribution.bash
+popd
+
+###
+#Agents
+###
+pushd projects/agents/randomAgentJava
+svn up
+bash makeDistribution.bash
+popd
+
