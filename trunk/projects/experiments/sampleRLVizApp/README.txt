816a1ea56a20fc4c0f8755fa6781bc9ab96e1385
brian@tannerpages.com <brian@tannerpages.com@b17b758d-1535-0410-a358-a1ba4bb9075e>
2009-03-07 21:46:20 +0000

    Made some changes to the distributions scripts to get the RLViz sample app working, and added that sample app.

---
+++ trunk/projects/experiments/sampleRLVizApp/README.txt
@@ -0,0 +1,38 @@
+##This is actually a template that has some variables in here to be replaced.  Lines starting with ## are comments.
+##Things like $VERSION$ are variables.
+##Substitutions are done using files in system/common/scripts/templates
+##
+#summary This is the page for download information and details about the $LANGUAGE$ $PROJECTTITLE$ Domain, Version: r$VERSION$.
+#labels Language-$LANGUAGE$,Type-$PROJECTTYPE$,Page-Download
+#sidebar $SIDEBAR$
+
+
+$WIKICOMMENTTEMPLATE$
+
+$INTRODUCTIONTEMPLATE$
+
+== Important Notes ==
+This experiment dynamically loads agents and/or environments from JAR files in your `rl-library/products` directory.
+For things to work right, it would be best if you had one of each in there.  We suggest (if you are starting out)  
+the [MountainCarJava] environment and [EpsilonGreedyTileCodingSarsaLambdaJava Tile Coding Sarsa Lambda Java] agent. 
+
+This experiment is actually part of the [GraphicalExperimentsWithRLViz graphical experiment getting started guide], so please consult that document for full 
+details and explanation.
+
+$USINGTHISDOWNLOADTEMPLATE$
+
+$RUNNINGTHISEXPERIMENTTEMPLATE$
+
+$HELPTEMPLATE$
+
+== Authors ==
+ * [http://research.tannerpages.com Brian Tanner]
+
+=== Current Maintainer ===
+ * [http://research.tannerpages.com Brian Tanner]
+ * btanner@rl-community.org
+ * [http://research.tannerpages.com]
+
+
+
+
---
+++ trunk/projects/experiments/sampleRLVizApp/makeDistribution.bash
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+
+#Make the distribution for the RL-Glue Sample experiment
+#This relies on a fairly general script that will work for most Java environments and agents.
+#Requirements
+#	this should be done with code that is in a subversion checkout
+#	you should have a build.xml file that follows the "rl-library build system"
+#	I'll add more as I think of it.
+
+#Maybe this should be done in ANT.  Not quite sure.
+
+#Set these variables for this specific project
+PROJECTTITLE='RLViz Application Sample Experiment'
+PROJECTNAME=SampleExperimentRLVizApp-Java
+SYSTEMPATH=../../../system
+WIKIPAGENAME=SampleExperimentRLVizAppJava
+SVNPASSWORDFILE=~/rl-library-svn-password
+PROJECTTYPE=Experiment
+LANGUAGE=Java
+HOMEURL=http://code.google.com/p/rl-library/wiki/SampleExperimentRLVizAppJava
+
+
+#Get all of the build functions
+source $SYSTEMPATH/common/scripts/build-java-experiment.sh
+
+#Sets up the most of the paths and creates the distribution directory
+#Copies the default set of jars and stuff to the appropriate directories.
+#After you call this, you should copy whatever other files you need to $DISTDIR
+javaDistributionInit
+
+cp $SYSTEMPATH/common/libs/rl-viz/*.jar $DISTDIR/system/common/libs/rl-viz/
+
+#
+#  Extra files you want to distribute go here
+#
+
+#This will go into $DISTDIR, build the project, remove the build directory, back out
+#tar and gzip the directory, and then delete the directory.
+javaDistributionBuildJarAndGzip
+
+if [ -z "$SKIPUPLOAD" ]
+then
+	javaDistributionUploadFile
+else
+	echo "   Skipping upload."
+fi
+
+
+javaDistributionUpdateWiki
+
+#Delete $DISTDIR and $DISTFILENAME
+#javaDistributionCleanup
